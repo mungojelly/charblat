d17d056cf768c14c60c0b4858aeef62c8b8ecfc6
Mungojelly <mungojelly@gmail.com>
2017-11-27 00:30:30 -0500

    bounces around a changing cloudy landscape

--- charblat.py
+++ charblat.py
@@ -3,7 +3,9 @@ import time
 import random
 
 
-REFRESH_TIME = 0.17
+REFRESH_TIME = 0.23
+PAD_HEIGHT = 500
+PAD_WIDTH = 700
 
 
 def twiddle(original, amount, minimum, maximum):
@@ -19,28 +21,38 @@ def main(stdscr):
     curses.curs_set(0)  # make cursor invisible
     stdscr.nodelay(1)  # don't wait for input when calling getch
     stdscr.clear()
-    pad = curses.newpad(1000, 1000)
+    pad = curses.newpad(PAD_HEIGHT, PAD_WIDTH)
     pad.nodelay(1)
-    for y in range(0, 999):
-        for x in range(0, 999):
+    for y in range(0, PAD_HEIGHT - 1):
+        for x in range(0, PAD_WIDTH - 1):
             pad.addch(y, x, ord('a') + (x*x+y*y) % 26)
-    current_column = 100
+    current_column = 0
+    column_direction = 1
+    current_row = 0
+    row_direction = 1
     time_refreshed = time.time()
     while True:
         if time.time() - time_refreshed > REFRESH_TIME:
             time_refreshed = time.time()
-            current_column = current_column + 1
-            if current_column > 900:
-                current_column = 100
             (maxy, maxx) = stdscr.getmaxyx()
-            pad.refresh(current_column, 0, 0, 0, maxy - 1, maxx - 1)
+            if (column_direction == 1) and ((current_column + maxx) >= PAD_WIDTH):
+                column_direction = 0 - column_direction
+            if (column_direction == -1) and (current_column == 0):
+                column_direction = 0 - column_direction
+            current_column = current_column + column_direction
+            if (row_direction == 1) and ((current_row + maxy) >= PAD_HEIGHT):
+                row_direction = 0 - row_direction
+            if (row_direction == -1) and (current_row == 0):
+                row_direction = 0 - row_direction
+            current_row = current_row + row_direction
+            pad.refresh(current_row, current_column, 0, 0, maxy - 1, maxx - 1)
         for i in range(1000):
-            origin_y = random.randint(0, 999)
-            origin_x = random.randint(0, 999)
-            target_y = twiddle(origin_y, 1, 0, 999)
-            target_x = twiddle(origin_x, 1, 0, 999)
+            origin_y = random.randint(0, PAD_HEIGHT - 1)
+            origin_x = random.randint(0, PAD_WIDTH - 1)
+            target_y = twiddle(origin_y, 1, 0, PAD_HEIGHT - 1)
+            target_x = twiddle(origin_x, 1, 0, PAD_WIDTH - 1)
             origin_char = pad.instr(origin_y, origin_x, 1)
-            if (target_y == 999) and (target_x == 999):
+            if (target_y == PAD_HEIGHT - 1) and (target_x == PAD_WIDTH - 1):
                 pass  # don't trigger this curses bug
             else:
                 pad.addch(target_y, target_x, origin_char)
