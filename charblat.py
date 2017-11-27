9b96de0c96f9a05eb5cb23b7ca71b3eed04129c1
Mungojelly <mungojelly@gmail.com>
2017-11-27 18:36:40 -0500

    still mostly boring diagonal lines

--- charblat.py
+++ charblat.py
@@ -1,144 +1,300 @@
 import curses
 import time
 import random
+import types
+import sys
 
 
 REFRESH_TIME = 0.25
-DRIFT_TIME = 1
-PROGRAM_CHANGE_TIME = 7
-PAD_HEIGHT = 150
-PAD_WIDTH = 250
-INITIAL_ENERGY = 600
-PROGRAM_LENGTH = 50
+DRIFT_TIME = 3
+PROGRAM_CHANGE_TIME = 3
+PAD_HEIGHT = 200
+PAD_WIDTH = 500
+INITIAL_ENERGY = 60000
+LOW_ENERGY = 6000
+PROGRAM_LENGTH = 30
 MAX_STACK_LENGTH = 30
+MAX_INSTRUCTION_STACK_LENGTH = 50
 
 
-instructions = ['up', 'down', 'left', 'right',
-                'put_a', 'put_b', 'put_c',
-                'push', 'pop',
-                'skip_if_c',
-                'shuffle_stack',
-                'turn_a_to_d',
-                'check_if_stack',
-                'skip_ten_if_g',
-                'up_unless_e',
-                'left_unless_e',
-                'empty_stack',
-                'double_stack',
-                ]
+all_instructions = ['up', 'down', 'left', 'right',
+                    'put_a', 'put_b', 'put_c', 'put_e', 'put_i',
+                    'put_k', 'put_l', 'put_m',
+                    'push', 'pop',
+                    'skip_if_c',
+                    'shuffle_stack',
+                    'turn_a_to_d',
+                    'check_if_stack',
+                    'skip_ten_if_g',
+                    'up_unless_e',
+                    'left_unless_e',
+                    'empty_stack',
+                    'double_stack',
+                    'h_if_low_energy',
+                    'skip_eight_if_h',
+                    'push_instruction',
+                    'pop_instruction',
+                    'pop_instruction_if_j',
+                    'i_to_j',
+                    'shuffle_instruction_stack',
+                    'move',
+                    'go_up', 'go_down', 'go_left', 'go_right',
+                    'k_up_l_down',
+                    'increase_counter',
+                    'decrease_counter',
+                    'pop_instruction_if_counter_zero',
+                    'reset_counter',
+                    'move_if_m',
+                    'remember_x', 'recall_x',
+                    'remember_y', 'recall_y',
+]
+
+distributions = [
+    ['up', 'up', 'down', 'down',
+     'left', 'left', 'left', 'left',
+     'right', 'right', 'right', 'right',
+     'h_if_low_energy', 'skip_eight_if_h',
+     'put_a',
+    ],
+    ['increase_counter', 'decrease_counter',
+     'pop_instruction_if_counter_zero',
+     'push_instruction', 'reset_counter',
+     'put_c', 'skip_if_c',
+     'put_i', 'i_to_j',
+     'pop_instruction_if_j',
+     'move', 'move', 'move',
+     'go_up', 'go_down',
+     'go_left', 'go_right',
+    ],
+]
 
 
 def random_program():
+    distribution = random.choice(distributions)
+    distribution = all_instructions
     program = []
     for instruction in range(PROGRAM_LENGTH):
-        program.append(random.choice(instructions))
+        program.append(random.choice(distribution))
     return program
 
 
-def run_instruction(program, current_instruction, pad, y, x, energy, stack):
-    if current_instruction >= len(program):
-        return (y, x, 0, stack)
-    instruction = program[current_instruction]
+def run_instruction(program, context, energy):
+    if context.current_instruction >= len(program):
+        context.current_instruction = 0
+        return context
+    instruction = program[context.current_instruction]
+    contents = context.pad.instr(context.y, context.x, 1)
     if instruction == 'up':
-        new_y = y - 1
+        new_y = context.y - 1
         if new_y < 0:
             new_y = PAD_HEIGHT - 2
-        return (new_y, x, current_instruction + 1, stack)
+        context.y = new_y
     if instruction == 'down':
-        new_y = y + 1
+        new_y = context.y + 1
         if new_y > PAD_HEIGHT - 2:
             new_y = 0
-        return (new_y, x, current_instruction + 1, stack)
+        context.y = new_y
     if instruction == 'left':
-        new_x = x - 1
+        new_x = context.x - 1
         if new_x < 0:
             new_x = PAD_WIDTH - 2
-        return (y, new_x, current_instruction + 1, stack)
+        context.x = new_x
     if instruction == 'right':
-        new_x = x + 1
+        new_x = context.x + 1
         if new_x > PAD_WIDTH - 2:
             new_x = 0
-        return (y, new_x, current_instruction + 1, stack)
+        context.x = new_x
     if instruction == 'put_a':
-        pad.addch(y, x, ord('a'))
-        return (y, x, current_instruction + 1, stack)
+        context.pad.addch(context.y, context.x, ord('a'))
     if instruction == 'put_b':
-        pad.addch(y, x, ord('b'))
-        return (y, x, current_instruction + 1, stack)
+        context.pad.addch(context.y, context.x, ord('b'))
     if instruction == 'put_c':
-        pad.addch(y, x, ord('c'))
-        return (y, x, current_instruction + 1, stack)
+        context.pad.addch(context.y, context.x, ord('c'))
     if instruction == 'put_e':
-        pad.addch(y, x, ord('e'))
-        return (y, x, current_instruction + 1, stack)
+        context.pad.addch(context.y, context.x, ord('e'))
+    if instruction == 'put_i':
+        context.pad.addch(context.y, context.x, ord('i'))
+    if instruction == 'put_k':
+        context.pad.addch(context.y, context.x, ord('k'))
+    if instruction == 'put_l':
+        context.pad.addch(context.y, context.x, ord('l'))
+    if instruction == 'put_m':
+        context.pad.addch(context.y, context.x, ord('m'))
     if instruction == 'push':
-        if len(stack) >= MAX_STACK_LENGTH:
-            return (y, x, current_instruction + 1, stack)
-        return (y, x, current_instruction + 1, stack + [(y, x)])
-    if instruction == 'pop':
-        if len(stack) == 0:
-            return (y, x, current_instruction + 1, stack)
-        (new_y, new_x) = stack.pop()
-        return (new_y, new_x, current_instruction + 1, stack)
-    if instruction == 'skip_if_c':
-        if pad.instr(y, x, 1) == b'c':
-            return (y, x, current_instruction + 2, stack)
-        return (y, x, current_instruction + 1, stack)
-    if instruction == 'shuffle_stack':
-        random.shuffle(stack)
-        return (y, x, current_instruction + 1, stack)
-    if instruction == 'turn_a_to_d':
-        if pad.instr(y, x, 1) == b'a':
-            pad.addch(y, x, ord('d'))
-        return (y, x, current_instruction + 1, stack)
-    if instruction == 'restart_if_e':
-        if pad.instr(y, x, 1) == b'e':
-            return (y, x, 0, stack)
-        return (y, x, current_instruction + 1, stack)
-    if instruction == 'check_if_stack':
-        if len(stack) == 0:
-            pad.addch(y, x, ord('f'))
+        if len(context.stack) >= MAX_STACK_LENGTH:
+            pass
         else:
-            pad.addch(y, x, ord('g'))
-        return (y, x, current_instruction + 1, stack)
+            context.stack = context.stack + [(context.y, context.x)]
+    if instruction == 'pop':
+        if len(context.stack) == 0:
+            pass
+        else:
+            (context.y, context.x) = context.stack.pop()
+    if instruction == 'skip_if_c':
+        if contents == b'c':
+            context.current_instruction += 1
+    if instruction == 'shuffle_stack':
+        random.shuffle(context.stack)
+    if instruction == 'turn_a_to_d':
+        if contents == b'a':
+            context.pad.addch(context.y, context.x, ord('d'))
+    if instruction == 'restart_if_e':
+        if contents == b'e':
+            context.current_instruction = -1
+    if instruction == 'check_if_stack':
+        if len(context.stack) == 0:
+            context.pad.addch(context.y, context.x, ord('f'))
+        else:
+            context.pad.addch(context.y, context.x, ord('g'))
     if instruction == 'skip_ten_if_g':
-        if pad.instr(y, x, 1) == b'g':
-            return (y, x, current_instruction + 11, stack)
-        return (y, x, current_instruction + 1, stack)
+        if contents == b'g':
+            context.current_instruction += 10
     if instruction == 'up_unless_e':
-        if pad.instr(y, x, 1) == b'e':
-            return (y, x, current_instruction + 1, stack)
-        new_y = y - 1
-        if new_y < 0:
-            new_y = PAD_HEIGHT - 2
-        return (new_y, x, current_instruction + 1, stack)
+        if contents == b'e':
+            pass
+        else:
+            new_y = context.y - 1
+            if new_y < 0:
+                new_y = PAD_HEIGHT - 2
+            context.y = new_y
     if instruction == 'left_unless_e':
-        if pad.instr(y, x, 1) == b'e':
-            return (y, x, current_instruction + 1, stack)
-        new_x = x - 1
-        if new_x < 0:
-            new_x = PAD_WIDTH - 2
-        return (y, new_x, current_instruction + 1, stack)
+        if contents == b'e':
+            pass
+        else:
+            new_x = context.x - 1
+            if new_x < 0:
+                new_x = PAD_WIDTH - 2
+            context.x = new_x
     if instruction == 'empty_stack':
-        return (y, x, current_instruction + 1, [])
+        context.stack = []
     if instruction == 'double_stack':
-        new_stack = stack + stack
-        while len(new_stack) > MAX_STACK_LENGTH:
-            new_stack.pop()
-        return (y, x, current_instruction + 1, new_stack)
+        context.stack = context.stack + context.stack
+        while len(context.stack) > MAX_STACK_LENGTH:
+            context.stack.pop()
+    if instruction == 'h_if_low_energy':
+        if energy < LOW_ENERGY:
+            context.pad.addch(context.y, context.x, ord('h'))
+    if instruction == 'skip_eight_if_h':
+        if contents == b'h':
+            context.current_instruction += 8
+    if instruction == 'push_instruction':
+        if len(context.instruction_stack) >= MAX_INSTRUCTION_STACK_LENGTH:
+            pass
+        else:
+            context.instruction_stack = context.instruction_stack + \
+                                        [context.current_instruction]
+    if instruction == 'pop_instruction':
+        if len(context.instruction_stack) == 0:
+            pass
+        else:
+            context.current_instruction = context.instruction_stack.pop()
+    if instruction == 'pop_instruction_if_j':
+        if len(context.instruction_stack) == 0:
+            pass
+        elif contents != b'j':
+            pass
+        else:
+            context.current_instruction = context.instruction_stack.pop()
+    if instruction == 'i_to_j':
+        if contents == b'i':
+            context.pad.addch(context.y, context.x, ord('j'))
+    if instruction == 'shuffle_instruction_stack':
+        random.shuffle(context.instruction_stack)
+    if instruction == 'move':
+        if context.direction == 'up':
+            new_y = context.y - 1
+            if new_y < 0:
+                new_y = PAD_HEIGHT - 2
+            context.y = new_y
+        if context.direction == 'down':
+            new_y = context.y + 1
+            if new_y > PAD_HEIGHT - 2:
+                new_y = 0
+            context.y = new_y
+        if context.direction == 'left':
+            new_x = context.x - 1
+            if new_x < 0:
+                new_x = PAD_WIDTH - 2
+            context.x = new_x
+        if context.direction == 'right':
+            new_x = context.x + 1
+            if new_x > PAD_WIDTH - 2:
+                new_x = 0
+            context.x = new_x
+    if instruction == 'move_if_m':
+        if contents == b'm':
+            if context.direction == 'up':
+                new_y = context.y - 1
+                if new_y < 0:
+                    new_y = PAD_HEIGHT - 2
+                context.y = new_y
+            if context.direction == 'down':
+                new_y = context.y + 1
+                if new_y > PAD_HEIGHT - 2:
+                    new_y = 0
+                context.y = new_y
+            if context.direction == 'left':
+                new_x = context.x - 1
+                if new_x < 0:
+                    new_x = PAD_WIDTH - 2
+                context.x = new_x
+            if context.direction == 'right':
+                new_x = context.x + 1
+                if new_x > PAD_WIDTH - 2:
+                    new_x = 0
+                context.x = new_x
+    if instruction == 'go_up':
+        context.direction = 'up'
+    if instruction == 'go_down':
+        context.direction = 'down'
+    if instruction == 'go_left':
+        context.direction = 'left'
+    if instruction == 'go_right':
+        context.direction = 'right'
+    if instruction == 'k_up_l_down':
+        if contents == b'k':
+            context.direction = 'up'
+        if contents == b'l':
+            context.direction = 'down'
+    if instruction == 'increase_counter':
+        context.counter += 1
+    if instruction == 'decrease_counter':
+        context.counter -= 1
+        if context.counter < 0:
+            context.counter = 0
+    if instruction == 'pop_instruction_if_counter_zero':
+        if len(context.instruction_stack) == 0:
+            pass
+        elif context.counter != 0:
+            pass
+        else:
+            context.current_instruction = context.instruction_stack.pop()
+    if instruction == 'reset_counter':
+        context.counter = 0
+    if instruction == 'remember_x':
+        context.remembered_x = context.x
+    if instruction == 'recall_x':
+        context.x = context.remembered_x
+        if (context.y == PAD_HEIGHT - 1) and (context.x == PAD_WIDTH - 1):
+            context.y = 0
+            context.x = 0
+            # don't trigger this curses bug
+    if instruction == 'remember_y':
+        context.remembered_y = context.y
+    if instruction == 'recall_y':
+        context.y = context.remembered_y
+        if (context.y == PAD_HEIGHT - 1) and (context.x == PAD_WIDTH - 1):
+            context.y = 0
+            context.x = 0
+            # don't trigger this curses bug
+    context.current_instruction += 1
+    return context
 
 
-def run_program(program, pad, y, x, current_instruction, energy, stack):
-    if energy == 0:
-        return True
-    (new_y, new_x, next_instruction, stack) = run_instruction(program,
-                                                              current_instruction,
-                                                              pad,
-                                                              y,
-                                                              x,
-                                                              energy,
-                                                              stack)
-    run_program(program, pad, new_y, new_x, next_instruction, energy - 1, stack)
+def run_program(program, context, energy):
+    while energy > 0:
+        energy = energy - 1
+        context = run_instruction(program, context, energy)
 
 
 def twiddle(original, amount, minimum, maximum):
@@ -193,13 +349,20 @@ def main(stdscr):
                 row_direction = 0 - row_direction
             current_row = current_row + row_direction
             pad.refresh(current_row, current_column, 0, 0, maxy - 1, maxx - 1)
+        context = types.SimpleNamespace()
+        context.pad = pad
+        context.y = random.randint(0, PAD_HEIGHT - 2)
+        context.x = random.randint(0, PAD_WIDTH - 2)
+        context.remembered_y = y
+        context.remembered_x = x
+        context.stack = []
+        context.current_instruction = random.randint(0, len(current_program))
+        context.instruction_stack = []
+        context.direction = 'right'
+        context.counter = 0
         run_program(current_program,
-                    pad,
-                    random.randint(0, PAD_HEIGHT - 2),
-                    random.randint(0, PAD_WIDTH - 2),
-                    0,
-                    INITIAL_ENERGY,
-                    [])
+                    context,
+                    INITIAL_ENERGY)
         c = pad.getch()
         if c == ord('q'):
             break
