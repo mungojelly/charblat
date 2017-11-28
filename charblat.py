6f35ac395b2e8b6cfdb2a82e79d5ced22d6dccd4
Mungojelly <mungojelly@gmail.com>
2017-11-27 19:38:54 -0500

    a few boring distributions

--- charblat.py
+++ charblat.py
@@ -1,20 +1,21 @@
 import curses
 import time
 import random
-import types
-import sys
+import copy
 
 
 REFRESH_TIME = 0.25
-DRIFT_TIME = 3
-PROGRAM_CHANGE_TIME = 3
+DRIFT_TIME = 1
+PROGRAM_CHANGE_TIME = 15
 PAD_HEIGHT = 200
 PAD_WIDTH = 500
 INITIAL_ENERGY = 60000
 LOW_ENERGY = 6000
-PROGRAM_LENGTH = 30
-MAX_STACK_LENGTH = 30
-MAX_INSTRUCTION_STACK_LENGTH = 50
+PROGRAM_LENGTH = 52
+MAX_STACK_LENGTH = 50
+MAX_INSTRUCTION_STACK_LENGTH = 20
+MAX_XY_STACK_LENGTH = 250
+MAX_CONTEXT_STACK_LENGTH = 120
 
 
 all_instructions = ['up', 'down', 'left', 'right',
@@ -47,6 +48,13 @@ all_instructions = ['up', 'down', 'left', 'right',
                     'move_if_m',
                     'remember_x', 'recall_x',
                     'remember_y', 'recall_y',
+                    'push_x_stack',
+                    'push_y_stack',
+                    'pop_x_stack',
+                    'pop_y_stack',
+                    'push_context',
+                    'pop_context',
+                    'move_countdown',
 ]
 
 distributions = [
@@ -66,24 +74,31 @@ distributions = [
      'go_up', 'go_down',
      'go_left', 'go_right',
     ],
+    ['move','move','go_left','go_right','up','down','push_context','pop_context',
+     'put_a', 'put_m', 'move_if_m', 'put_i', 'i_to_j', 'put_c', 'skip_if_c',
+     'h_if_low_energy', 'skip_eight_if_h', 'move_countdown', 'increase_counter',
+    ]
 ]
+
+
+
 
 
 def random_program():
     distribution = random.choice(distributions)
-    distribution = all_instructions
+    # distribution = all_instructions
     program = []
     for instruction in range(PROGRAM_LENGTH):
         program.append(random.choice(distribution))
     return program
 
 
-def run_instruction(program, context, energy):
+def run_instruction(program, pad, context, energy):
     if context.current_instruction >= len(program):
         context.current_instruction = 0
         return context
     instruction = program[context.current_instruction]
-    contents = context.pad.instr(context.y, context.x, 1)
+    contents = pad.instr(context.y, context.x, 1)
     if instruction == 'up':
         new_y = context.y - 1
         if new_y < 0:
@@ -105,21 +120,21 @@ def run_instruction(program, context, energy):
             new_x = 0
         context.x = new_x
     if instruction == 'put_a':
-        context.pad.addch(context.y, context.x, ord('a'))
+        pad.addch(context.y, context.x, ord('a'))
     if instruction == 'put_b':
-        context.pad.addch(context.y, context.x, ord('b'))
+        pad.addch(context.y, context.x, ord('b'))
     if instruction == 'put_c':
-        context.pad.addch(context.y, context.x, ord('c'))
+        pad.addch(context.y, context.x, ord('c'))
     if instruction == 'put_e':
-        context.pad.addch(context.y, context.x, ord('e'))
+        pad.addch(context.y, context.x, ord('e'))
     if instruction == 'put_i':
-        context.pad.addch(context.y, context.x, ord('i'))
+        pad.addch(context.y, context.x, ord('i'))
     if instruction == 'put_k':
-        context.pad.addch(context.y, context.x, ord('k'))
+        pad.addch(context.y, context.x, ord('k'))
     if instruction == 'put_l':
-        context.pad.addch(context.y, context.x, ord('l'))
+        pad.addch(context.y, context.x, ord('l'))
     if instruction == 'put_m':
-        context.pad.addch(context.y, context.x, ord('m'))
+        pad.addch(context.y, context.x, ord('m'))
     if instruction == 'push':
         if len(context.stack) >= MAX_STACK_LENGTH:
             pass
@@ -137,15 +152,15 @@ def run_instruction(program, context, energy):
         random.shuffle(context.stack)
     if instruction == 'turn_a_to_d':
         if contents == b'a':
-            context.pad.addch(context.y, context.x, ord('d'))
+            pad.addch(context.y, context.x, ord('d'))
     if instruction == 'restart_if_e':
         if contents == b'e':
             context.current_instruction = -1
     if instruction == 'check_if_stack':
         if len(context.stack) == 0:
-            context.pad.addch(context.y, context.x, ord('f'))
+            pad.addch(context.y, context.x, ord('f'))
         else:
-            context.pad.addch(context.y, context.x, ord('g'))
+            pad.addch(context.y, context.x, ord('g'))
     if instruction == 'skip_ten_if_g':
         if contents == b'g':
             context.current_instruction += 10
@@ -173,7 +188,7 @@ def run_instruction(program, context, energy):
             context.stack.pop()
     if instruction == 'h_if_low_energy':
         if energy < LOW_ENERGY:
-            context.pad.addch(context.y, context.x, ord('h'))
+            pad.addch(context.y, context.x, ord('h'))
     if instruction == 'skip_eight_if_h':
         if contents == b'h':
             context.current_instruction += 8
@@ -197,7 +212,7 @@ def run_instruction(program, context, energy):
             context.current_instruction = context.instruction_stack.pop()
     if instruction == 'i_to_j':
         if contents == b'i':
-            context.pad.addch(context.y, context.x, ord('j'))
+            pad.addch(context.y, context.x, ord('j'))
     if instruction == 'shuffle_instruction_stack':
         random.shuffle(context.instruction_stack)
     if instruction == 'move':
@@ -223,6 +238,31 @@ def run_instruction(program, context, energy):
             context.x = new_x
     if instruction == 'move_if_m':
         if contents == b'm':
+            if context.direction == 'up':
+                new_y = context.y - 1
+                if new_y < 0:
+                    new_y = PAD_HEIGHT - 2
+                context.y = new_y
+            if context.direction == 'down':
+                new_y = context.y + 1
+                if new_y > PAD_HEIGHT - 2:
+                    new_y = 0
+                context.y = new_y
+            if context.direction == 'left':
+                new_x = context.x - 1
+                if new_x < 0:
+                    new_x = PAD_WIDTH - 2
+                context.x = new_x
+            if context.direction == 'right':
+                new_x = context.x + 1
+                if new_x > PAD_WIDTH - 2:
+                    new_x = 0
+                context.x = new_x
+    if instruction == 'move_countdown':
+        context.counter -= 1
+        if context.counter < 0:
+            context.counter = 0
+        else:
             if context.direction == 'up':
                 new_y = context.y - 1
                 if new_y < 0:
@@ -287,14 +327,44 @@ def run_instruction(program, context, energy):
             context.y = 0
             context.x = 0
             # don't trigger this curses bug
+    if instruction == 'push_x_stack':
+        if len(context.x_stack) >= MAX_XY_STACK_LENGTH:
+            pass
+        else:
+            context.x_stack = context.x_stack + [context.x]
+    if instruction == 'push_y_stack':
+        if len(context.y_stack) >= MAX_XY_STACK_LENGTH:
+            pass
+        else:
+            context.y_stack = context.y_stack + [context.y]
+    if instruction == 'pop_x_stack':
+        if len(context.x_stack) == 0:
+            pass
+        else:
+            context.x = context.x_stack.pop()
+    if instruction == 'pop_y_stack':
+        if len(context.y_stack) == 0:
+            pass
+        else:
+            context.x = context.y_stack.pop()
+    if instruction == 'push_context':
+        if len(context.context_stack) >= MAX_CONTEXT_STACK_LENGTH:
+            pass
+        else:
+            context.context_stack = context.context_stack + [copy.deepcopy(context)]  # lol
+    if instruction == 'pop_context':
+        if len(context.context_stack) == 0:
+            pass
+        else:
+            context = context.context_stack.pop()
     context.current_instruction += 1
     return context
 
 
-def run_program(program, context, energy):
+def run_program(program, pad, context, energy):
     while energy > 0:
         energy = energy - 1
-        context = run_instruction(program, context, energy)
+        context = run_instruction(program, pad, context, energy)
 
 
 def twiddle(original, amount, minimum, maximum):
@@ -304,6 +374,10 @@ def twiddle(original, amount, minimum, maximum):
     if twiddled > maximum:
         twiddled = maximum
     return twiddled
+
+
+class Context(object):
+    pass
 
 
 def main(stdscr):
@@ -349,8 +423,7 @@ def main(stdscr):
                 row_direction = 0 - row_direction
             current_row = current_row + row_direction
             pad.refresh(current_row, current_column, 0, 0, maxy - 1, maxx - 1)
-        context = types.SimpleNamespace()
-        context.pad = pad
+        context = Context()
         context.y = random.randint(0, PAD_HEIGHT - 2)
         context.x = random.randint(0, PAD_WIDTH - 2)
         context.remembered_y = y
@@ -360,7 +433,11 @@ def main(stdscr):
         context.instruction_stack = []
         context.direction = 'right'
         context.counter = 0
+        context.x_stack = []
+        context.y_stack = []
+        context.context_stack = []
         run_program(current_program,
+                    pad,
                     context,
                     INITIAL_ENERGY)
         c = pad.getch()
